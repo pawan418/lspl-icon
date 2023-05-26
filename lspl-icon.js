4c898ff2b1eaa696e4ad679ce32e3f0bbda294f5
Pawan Kumar Singh <pawan0890@gmail.com>
2023-05-26 06:29:19 +0530

    Update lspl-icon.js

--- lspl-icon.js
+++ lspl-icon.js
@@ -28,7 +28,7 @@ function insertIcon(selector) {
 				size = classParts[2];
 			}
 		}
-		var svgURL = "https://pawan418.github.io/lspl-icon/lspl-icon.svg";
+		var svgURL = "lspl-icon.svg";
         // Fetch the SVG file from the provided URL
         fetch(svgUrl)
 		// Fetch the SVG file from the "css" folder on your server
